9c51e1a5020cc07753763a329705ac57abd7e536
Six-of-Sheep <272097372+Six-of-Sheep@users.noreply.github.com>
2026-09-22 16:41:27 +0800

    Bind revision 31 execution identity to bridge034

--- tools/verify_v2b_frozen_launch_plan_r31.py
+++ tools/verify_v2b_frozen_launch_plan_r31.py
@@ -7,6 +7,9 @@ import verify_v2b_frozen_launch_plan_r30 as _r30
 
 _r30.AUTH_ID = "smoke-v2b-rev1-s2-r896-bridge-034"
 _r30.PLAN_ID = "P3-V2B-REV1-FROZEN-LAUNCH-BRIDGE034"
+_r30.BRIDGE_SHA = "1079e52acb3637d2a368f315f50c15a49d2f868bb9d302f078beedbffcb7b48f"
+_r30.BINDING_SHA = "b1b1462d1c13d0e08ac53cadf85cb88c19d53e65f86ec444054063b2dda9d021"
+_r30.MANIFEST_SHA = "5e10f12830b1fe601f7b06470fb8a5a2728b611c6a058369440a9104f6f5ac14"
 
 
 if __name__ == "__main__":
